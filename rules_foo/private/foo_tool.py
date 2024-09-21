753ee9243640718fda13a4aea9b767adfb1301bc
Greg Roodt <groodt@gmail.com>
2024-09-21 13:28:12 +1000

    Actual repro

--- rules_foo/private/foo_tool.py
+++ rules_foo/private/foo_tool.py
@@ -6,16 +6,17 @@ import sysconfig
 
 from pathlib import Path
 
+from typing_extensions import Annotated
+
+
+class MyModel(pydantic.BaseModel):
+    name: str = "Tweed"
+
 
 if __name__ == '__main__':
-    pydantic_version = pydantic.__version__
-    os_name = os.name
-    platform_machine = platform.machine()
-    sysconfig_platform = sysconfig.get_platform()
 
+    model = MyModel(name = "Jacket")
     output_path = sys.argv[1]
+
     with open(output_path, 'w') as f:
-        f.write(f"pydantic_version: {pydantic_version}\n")
-        f.write(f"os_name: {os_name}\n")
-        f.write(f"platform_machine: {platform_machine}\n")
-        f.write(f"sysconfig_platform: {sysconfig_platform}\n")
+        f.write(str(model.dict()))
